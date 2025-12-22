d35f86fce85542ed12ead44d5668fca300440979
K Kevin Gladson <joeljoyson2427@gmail.com>
2025-12-22 21:41:55 +0530

    made some logoin

--- small_pizza/android/build.gradle.kts
+++ small_pizza/android/build.gradle.kts
@@ -1,3 +1,15 @@
+// ✅ buildscript must be FIRST
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // ✅ Use double quotes for Kotlin DSL (not single quotes)
+        classpath("com.google.gms:google-services:4.4.1")
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -6,12 +18,13 @@ allprojects {
 }
 
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
+rootProject.layout.buildDirectory.set(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
+    project.layout.buildDirectory.set(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
